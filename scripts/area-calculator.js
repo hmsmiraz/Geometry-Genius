09a24ce2ecd6c7fb9329de7548481787d9e4d076
hmsmiraz <hmsmiraz64729@gmail.com>
2023-08-19 14:42:29 +0600

    edit

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -1,3 +1,37 @@
+// Reuseable function to parse value to float
+function getInputValue(fieldId){
+    const inputField = document.getElementById(fieldId);
+    const inputValueText = inputField.value;
+    const value = parseFloat(inputValueText);
+    return value;
+}
+// reusable function for set inner text
+function setInnerText(elementId, area){
+    const element = document.getElementById(elementId);
+    element.innerText = area;
+}
+
+// add to entry function
+function addToEntry(areaType, area){
+    console.log(areaType+ ' '  + area)
+    const calculationEntry = document.getElementById('calculation-entry');
+
+    const count = calculationEntry.childElementCount;
+
+    const p = document.createElement('p');
+    p.classList.add('my-4');
+    p.innerHTML = `${count + 1}. ${areaType} ${area} cm<sup>2</sup> 
+    <button class="btn btn-sm btn-success">Convert</button>`;
+
+    calculationEntry.appendChild(p);
+}
+// validation
+// function checkNan(fieldOne, fieldTwo){
+//     if(isNaN(fieldOne) || isNaN(fieldTwo)){
+//         alert('Please provide a number');     
+//     }
+//     return; 
+// }
 function calculateTriangleArea(){
     // get base value
     const baseField = document.getElementById('triangle-base');
@@ -35,20 +69,14 @@ function calculateRectangleArea(){
     areaSpan.innerText = rectangleArea;
 }
 function calculateParallelogramArea(){
-    // get base value
-    const baseField = document.getElementById('parallelogram-base');
-    const baseValueText = baseField.value;
-    const base = parseFloat(baseValueText);
-
-    // get height value
-    const heightField = document.getElementById('parallelogram-height');
-    const heightValueText = heightField.value;
-    const height = parseFloat(heightValueText);
-    console.log(base,height);
-
+    const base = getInputValue('parallelogram-base');
+    const height = getInputValue('parallelogram-height');
+    if(isNaN(base) || isNaN(height)){
+        alert('Please provide a number');
+        return;      
+    };
     const parallelogramArea = base * height;
-    console.log(parallelogramArea);
-
-    const areaSpan = document.getElementById('parallelogram-area');
-    areaSpan.innerText = parallelogramArea;
+    setInnerText('parallelogram-area',parallelogramArea);
+    addToEntry('Parallelogram', parallelogramArea);
 }
+
